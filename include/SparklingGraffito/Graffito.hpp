93324e47f99c5971f7b9cf21dd176040b1b6d38e
jacob-work <jacobbishopxy@gmail.com>
2025-07-09 20:58:43 +0800

    🎉 init

---
+++ include/SparklingGraffito/Graffito.hpp
@@ -0,0 +1,11 @@
+/**
+ * @file:	Graffito.hpp
+ * @author:	Jacob Xie
+ * @date:	2025/07/09 14:23:29 Wednesday
+ * @brief:
+ **/
+
+#ifndef __GRAFFITO__H__
+#define __GRAFFITO__H__
+
+#endif  //!__GRAFFITO__H__
---
+++ src/Graffito.cpp
@@ -0,0 +1,7 @@
+/**
+ * @file:	Graffito.cpp
+ * @author:	Jacob Xie
+ * @date:	2025/07/09 14:26:35 Wednesday
+ * @brief:
+ **/
+
